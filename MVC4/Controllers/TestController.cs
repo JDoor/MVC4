f61d2242d6cc3bcdd79a1ac04a75eaadc3cdba14
Josef Doornink <josefd@onfulfillment.com>
2017-09-19 23:06:32 -0700

    test

---
+++ MVC4/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC4.Models;
+
+namespace MVC4.Controllers
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+    }
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult GetView()
+        {
+            //return View("MyView");
+            Employee emp = new Employee();
+            emp.FirstName = "Sukesh";
+            emp.LastName = "Marla";
+            emp.Salary = 20000;
+            ViewData["Employee"] = emp;
+            return View("MyView");
+        }
+
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "Customer 1";
+            c.Address = "Address1";
+            return c;
+        }
+
+
+    }    
+}
